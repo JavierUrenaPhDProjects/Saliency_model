3d4da29531d9ee0378540deab8882ec377cedf30
JavierUrenaPhDProjects <114744054+JavierUrenaPhDProjects@users.noreply.github.com>
2023-11-29 16:45:02 +0100

    edited the script so now it plots whenever there is a change in the file. Also it only plots every n elements so it does an interpolation and the plot looks nicer

--- toolbox/cmd_plotting.py
+++ toolbox/cmd_plotting.py
@@ -3,15 +3,32 @@ import numpy as np
 import os
 import time
 
-clear = lambda: os.system('cls')
+clearscreen = lambda: os.system('clear')
+modelname = 'SalClass_embedd'
+n = 10
 
+
+def check_change(new_arr, old_arr):
+    return not np.array_equal(new_arr, old_arr)
+
+
+old_arr = []
 try:
     while True:
-        clear()
-        fig = plotille.Figure()
-        arr = np.load('../trained_models/SalClass_embedd/logs/loss.npy')
-        fig.plot(np.arange(len(arr)), arr, lc=200, label='line de prueba')
-        print(fig.show(legend=True))
-        time.sleep(2)
+        try:
+            new_arr = np.load(f'../trained_models/{modelname}/logs/loss.npy')
+        except EOFError:
+            print("got an EOFError....")
+            pass
+        if check_change(new_arr, old_arr) and len(new_arr) % n == 0:
+            clearscreen()
+            y = np.average(new_arr.reshape(-1, n), axis=1)
+            x = np.arange(len(y))
+            plot = plotille.plot(x, y, interp='linear', lc='red')
+            print(plot)
+        else:
+            pass
+        old_arr = new_arr
+        time.sleep(1)
 except KeyboardInterrupt:
     pass
