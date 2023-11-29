e2b4ee30a059a9eac1d976e71410a2a88c14bebd
JavierUrenaPhDProjects <114744054+JavierUrenaPhDProjects@users.noreply.github.com>
2023-11-29 16:46:21 +0100

    Added new SalClass_embedd model. This one uses two cnn backbones. Also aded an inference test script so it calculates the average inference time of the architecture

--- models/SalClass/SalClass_EmbedFusion.py
+++ models/SalClass/SalClass_EmbedFusion.py
@@ -1,6 +1,6 @@
 import torch
 from torch import nn
-from models.SalClass.parts.backbone import resnet_backbone
+from models.SalClass.parts.backbone import resnet_backbone, medium_cnn
 from models.SalClass.parts.vision_transformer import ViT
 
 
@@ -9,12 +9,25 @@ class SalClass(nn.Module):
     # This model will receive an embedding instead of an image. This means that the shape of the tensor
     # is going to be something like [16, 2048, 12, 12] where 2048 is the embedding dimensionality.
     # If we consider that as an image of 12x12 and 2048 channels we can configure the ViT that way
-    def __init__(self, emb_shape=12, patch_size=1, channels=2048, num_classes=257, dim=768, depth=12, heads=12,
-                 mlp_dim=2048, dropout=0.1, emb_dropout=0.1):
+    def __init__(self, channels=2048, num_classes=257, dim=768, depth=12, heads=12,
+                 mlp_dim=2048, dropout=0.1, emb_dropout=0.1, backbone='resnet'):
         super().__init__()
-        self.backbone = resnet_backbone()
-        self.backbone.eval()
-        self.vit = ViT(emb_shape, patch_size, num_classes, dim, depth, heads, mlp_dim,
+        self.backbone_type = backbone
+
+        if backbone == 'resnet':
+            self.emb_shape = 12
+            self.patch_size = 1
+            self.backbone = resnet_backbone()
+            self.backbone.eval()
+            self.common_bb = True
+        elif backbone == 'cnn':
+            self.emb_shape = 12  # n "pixels" the embedding has after the cnn
+            self.patch_size = 1  # emb_shape / patch_size = n_embeddings the ViT will have (48/4 = 12)
+            self.backbone1 = medium_cnn(n_channels=3)
+            self.backbone2 = medium_cnn(n_channels=1)
+            self.common_bb = False
+
+        self.vit = ViT(self.emb_shape, self.patch_size, num_classes, dim, depth, heads, mlp_dim,
                        channels=channels, dropout=dropout, emb_dropout=emb_dropout)
         self.vit.double()
         self.final = nn.Softmax(dim=1)
@@ -24,16 +37,16 @@ class SalClass(nn.Module):
         :param x1: this should be the image
         :param x2: this should be the saliency
         """
-        # try:
-        #     assert x2.shape[1] == 3
-        # except:
-        #     x2 = torch.cat((x2, x2, x2), dim=1)
 
-        if x2.shape[1] != 3:
-            x2 = torch.cat((x2, x2, x2), dim=1)
+        if self.common_bb:
+            if x2.shape[1] != 3:
+                x2 = torch.cat((x2, x2, x2), dim=1)
 
-        x1 = self.backbone(x1)
-        x2 = self.backbone(x2)
+            x1 = self.backbone(x1)
+            x2 = self.backbone(x2)
+        else:
+            x1 = self.backbone1(x1)
+            x2 = self.backbone2(x2)
 
         x = x1 + x2
 
@@ -42,6 +55,33 @@ class SalClass(nn.Module):
         return x
 
 
-def SalClass_embedd(args):
-    model = SalClass(dropout=args['dropout'])
+def SalClass_embedd_resnet(args):
+    model = SalClass(dropout=args['dropout'], backbone='resnet')
     return model
+
+
+def SalClass_embedd_cnn(args):
+    model = SalClass(dropout=args['dropout'], backbone='cnn')
+    return model
+
+
+if __name__ == '__main__':
+    x1 = torch.randn(1, 3, 384, 384).double()
+    x2 = torch.randn(1, 1, 384, 384).double()
+    measure_time = True
+
+    model = SalClass(dropout=0.001, backbone='cnn')
+    with torch.inference_mode():
+        model.eval()
+        y = model(x1, x2)
+        print(f' Output shape {y.shape}')
+        time_array = []
+        if measure_time:
+            import numpy as np
+            import time
+
+            for i in range(100):  # tome of 20 iterations in inference
+                time_init = time.time()
+                y = model(x1, x2)
+                time_array = np.append(time_array, time.time() - time_init)
+                print(f' Average Inference time over {i} iterations: {np.average(time_array)}')
